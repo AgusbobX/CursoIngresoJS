5776aeb774e2d8393c448f8d30dcfce335f3bf7c
AgusbobX <agusbob168@gmail.com>
2019-02-08 09:47:25 -0300

    Casi TERMINADO

--- 2-InstruccionIf/jsInstruccionIF (5).js
+++ 2-InstruccionIf/jsInstruccionIF (5).js
@@ -1,6 +1,8 @@
 function mostrar()
 {
 //tomo la edad  
-
-
+var edad = parseInt(document.getElementById("edad").value)
+if  (!(edad >= 13 && edad <= 17)) { 
+    alert("No sos un adolecente")
+}
 }//FIN DE LA FUNCIÓN
