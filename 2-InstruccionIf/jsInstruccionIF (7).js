647b6ee05c886613fdf41ecadbf296280012278c
AgusbobX <agusbob168@gmail.com>
2019-02-08 10:16:47 -0300

    Casi TERMINADO

--- 2-InstruccionIf/jsInstruccionIF (7).js
+++ 2-InstruccionIf/jsInstruccionIF (7).js
@@ -1,8 +1,10 @@
 function mostrar()
 {
 //tomo la edad  
+var edad = parseInt(document.getElementById("edad").value);
+var estadoCivil = document.getElementById("estadoCivil").value;
 
-	
-
-
+if (edad <= 18 && edad >= 0 && estadoCivil == "Casado" || edad <= 18 && edad >= 0 && estadoCivil == "Divorciado") {
+    alert("Es muy chiquito para NO ser soltero");
 }//FIN DE LA FUNCIÓN
+}
