ac192c1d66445aaafe6c693c366415a2d9533f49
AgusbobX <agusbob168@gmail.com>
2019-02-08 09:20:24 -0300

    Editado

--- 2-InstruccionIf/jsInstruccionIF (4).js
+++ 2-InstruccionIf/jsInstruccionIF (4).js
@@ -1,7 +1,8 @@
 function mostrar()
 {
 //tomo la edad  
-
-
-
+var edad = parseInt(document.getElementById("edad").value);
+if (edad >= 13 && edad <= 17) {
+    alert("Sos un adolecente")
+}
 }//FIN DE LA FUNCIÓN
