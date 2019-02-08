42a43338d1747320d51258babe6fb79c89d11482
AgusbobX <agusbob168@gmail.com>
2019-02-08 12:13:21 -0300

    INICIO Switch

--- 3-InstruccionSwitch/jsInstruccionSwitch(1).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(1).js
@@ -2,8 +2,21 @@ function mostrar()
 {
 //tomo la edad  
 var mesDelAño = document.getElementById('mes').value;
-
-
-
-
+switch(mesDelAño)
+{
+    case "Enero":
+        alert("¡Que comiences bien el año!");
+        break;
+    case "Marzo":
+        alert("¡A clases!");
+        break;
+    case "Julio":
+        alert("¡Se vinen las vacaciones!")
+        break;
+    case "Diciembre":
+        alert("¡Felices Fiestas!")
+        break;
+    default:
+        alert("No pasa nada interesante")
+}
 }//FIN DE LA FUNCIÓN
