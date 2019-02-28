dbe180fdf2f97fc90ab7decf118aa8910736b12c
AgusbobX <agusbob168@gmail.com>
2019-02-28 09:34:01 -0300

    Switch CASI Terminado

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -1,11 +1,26 @@
 function mostrar()
 {
-//tomo la edad  
-var mesDelAño = document.getElementById('mes').value;
+//tomo la edad
+var mesDelAño = document.getElementById("mes").value;
 
-alert (mesDelAño);
-
-
-
-
+switch(mesDelAño) {
+    case "Junio":
+    case "Julio" :
+    case "Agosto":
+    case "Septiembre":
+        alert("¡Abrigate que hace FRIO!");
+        break;
+    case "Octubre":
+    case "Noviembre":
+    case "Diciembre":
+        alert("Falta para el invierno!");
+        break;
+    case "Enero":
+    case "Febrero":
+    case "Marzo":
+    case "Abril":
+    case "Mayo":
+        alert("Ya pasamos el frio, ahora el calor!")
+        break;
+}
 }//FIN DE LA FUNCIÓN
--- 5-InstruccionWhile/jsIteraciones (7).js
+++ 5-InstruccionWhile/jsIteraciones (7).js
@@ -4,9 +4,11 @@ function mostrar()
 	var contador=0;
 	var acumulador=0;
 	var respuesta='si';
-
-
 document.getElementById('suma').value=acumulador;
 document.getElementById('promedio').value=acumulador/contador;
+while(contador < 5) {
+  contador++;
+
 
 }//FIN DE LA FUNCIÓN
+}
