2bb73442aed9ac2859ee0a8e9ad31f7e744403e2
AgusbobX <agusbob168@gmail.com>
2019-02-28 10:32:24 -0300

    SWITCH en PROCESO

--- 3-InstruccionSwitch/jsInstruccionSwitch(4).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(4).js
@@ -1,12 +1,27 @@
 function mostrar()
 {
-//tomo la edad  
+//tomo la edad
 var mesDelAño = document.getElementById('mes').value;
 
 //alert (mesDelAño);
-	
-	
-
-
-
+  switch(mesDelAño) {
+    case "Enero":
+    case "Marzo":
+    case "Mayo":
+    case "Julio":
+    case "Agosto":
+    case "Octubre":
+    case "Diciembre":
+      alert("Tiene 31 dias")
+      break;
+    case "Abril":
+    case "Junio":
+    case "Septiembre":
+    case "Noviembre":
+      alert("Tiene 30 dias");
+      break;
+    case "Febrero":
+      alert("Tiene 29 dias");
+      break;
+  }
 }//FIN DE LA FUNCIÓN
