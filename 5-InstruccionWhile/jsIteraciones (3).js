97d15d4140ae378586b0d966370b6a3f9c6535a3
AgusbobX <agusbob168@gmail.com>
2019-02-28 12:02:19 -0300

    VARIADOS

--- 5-InstruccionWhile/jsIteraciones (3).js
+++ 5-InstruccionWhile/jsIteraciones (3).js
@@ -1,32 +1,10 @@
 function mostrar()
 {
-
-//var clave = prompt("ingrese el número clave.");
-	var clave;
-	var intentos;
-	var flag;
-
-	intentos=0;
-	flag=false;
-	clave=prompt("Ingrese el numero clave");
-
-	while(clave!="utn750")
-	{
-		alert("Clave incorrecta");
-		clave=prompt("Ingrese el numero clave");
-		intentos=intentos+1;
-		if(intentos==2)
-		{
-			flag=true;
-			alert("Clave incorrecta mas de tres veces");
-			break;
-		}
-	}
-	if(flag==false)
-	{
-		alert("Bienvenido al sistema");
-	}
-
-
-
-}//FIN DE LA FUNCIÓN
+  var clave = prompt("Ingrese la Clave");
+  while(clave != "utn750")
+  {
+    alert("Vuelva a Ingresar la Clave");
+    clave = prompt("Ingrese la Clave");
+  }
+  alert("Bienvenido al sistema");
+  }//FIN DE LA FUNCIÓN
--- 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
+++ 9-Parciales/2018_1Cuatri_Parcial_fuentes/siete.js
@@ -1,4 +1,47 @@
 function mostrar()
 {
+var contador = 0;
+var totalnotas = 0;
+var sexo;
+var promedio;
+var notabaja;
+var varonesnota6 = 0;
+var nota;
 
+	while(contador < 5)
+	{
+		contador++;
+		nota = prompt("Ingresar nota del alumno "+contador);
+		nota = parseInt(nota);
+		while(nota<0 || nota>10)
+		{
+			nota = prompt("Reingrese la nota entre 0 y 10 para el alumno "+contador);
+			nota = parseInt(nota);
+		}
+		if(contador == 1)
+		{
+      notabaja = nota;
+    }
+		else {
+			if(nota < notabaja)
+			{
+        notabaja = nota;
+      }
+		}
+        sexo=prompt("Ingresar sexo del alumno con las letras F o M");
+		while(sexo !="F" && sexo !="M")
+		{
+			sexo = prompt("Reingrese el sexo del alumno (F o M)");
+		}
+		if(nota >= 6 && sexo == "M")
+		{
+			varonesnota6++;
+		}
+    totalnotas = totalnotas+nota;
+    totalnotas = parseInt(totalnotas);
+		promedio = totalnotas/contador;
+	}
+	alert("El promedio de las notas totales es: "+promedio);
+	alert("La nota mas baja es: "+notabaja);
+	alert("Varones con nota mayor o igual a 6: "+varonesnota6);
 }
